60328c1a6a981afd83e29e03dd1e443aa7d080db
Fly me to the moon <git@bigly.dog>
2020-06-17 11:33:29 -0700

    build js

---
+++ build.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const { spawnSync } = require("child_process");
+
+process.chdir(__dirname);
+
+spawnSync("sass", ["src/entry.scss", "dist/entry.css"], {
+  stdio: ["inherit", "inherit", "inherit"],
+});
+
